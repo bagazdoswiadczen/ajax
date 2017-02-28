d35e64df2fede8a5b6dc6f3baf072ee997d2e3ca
Michał Szymczyk <michalszymczyk@MacBook-Air-Michal-2.local>
2017-02-28 22:27:46 +0100

    add

---
+++ nation.js
@@ -0,0 +1,25 @@
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
+
+$('#search').click(searchCountries);
+
+function searchCountries() {
+ 	var countryName = $('#country-name').val();
+if(!countryName.length) countryName = 'Poland';
+$.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+}
+
+function showCountriesList(resp) {
+  countriesList.empty();
+}
+
+function showCountriesList(resp) {
+  	countriesList.empty();
+resp.forEach(function(item) {
+   		$('<li>').text(item.name).appendTo(countriesList);
+});
+}
